6e5f138ae8d426acfd260db086fed3a264fec630
vq0599 <vq0599@outlook.com>
2018-12-02 14:14:22 +0800

    hide db password

--- conf/init.go
+++ conf/init.go
@@ -4,7 +4,7 @@ const (
   DB_HOST       = "127.0.0.1:3306"
   DB_USER       = "root"
   DB_TYPE       = "mysql"
-  DB_PASSWORD   = "ceoms999"
+  DB_PASSWORD   = ""
   DB_NAME       = "blog"
 
   SERVER_PORT   = "8180"

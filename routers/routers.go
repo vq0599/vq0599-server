4836a6aa7ab3a81ae9139de27a398e87a7df037d
huangdehao <vq0599@outlook.com>
2019-03-08 10:42:34 +0800

    🐛 Fix router path

--- routers/routers.go
+++ routers/routers.go
@@ -21,14 +21,14 @@ func InitRouter() *gin.Engine {
   apiAdmin.POST("/login", controller.Login)
   apiAdmin.GET("/login", controller.GetLoginStatus)
 
-  api.Use(middleware.Jwt())
+  apiAdmin.Use(middleware.Jwt())
   {
     apiAdmin.GET("/articles", controller.GetArticles(true))
     apiAdmin.GET("/articles/:id", controller.GetArticle(true))
     apiAdmin.POST("/articles", controller.AddArticle)
     apiAdmin.DELETE("/articles/:id", controller.DeleteArticle)
     apiAdmin.PATCH("/articles/:id", controller.UpdateArticle)
-    apiAdmin.POST("/upload/image", controller.UploadImage)
+    apiAdmin.POST("/upload/images", controller.UploadImage)
   }
 
   return router
